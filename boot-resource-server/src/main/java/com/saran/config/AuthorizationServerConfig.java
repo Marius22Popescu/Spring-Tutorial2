3e305f5eeaa40ae7b64743a0ac3f0adaa6f13788
Marius Popescu <7.mariuspopescu.10@gmail.com>
2019-05-08 22:47:42 -0400

    Add files via upload

---
+++ boot-resource-server/src/main/java/com/saran/config/AuthorizationServerConfig.java
@@ -0,0 +1,20 @@
+package com.saran.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends 
+AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) 
+    		throws Exception {
+        clients.inMemory().withClient("Emre").secret("123")
+        .authorizedGrantTypes("authorization_code")
+            .scopes("read").authorities("CLIENT");
+    }
+}
---
+++ boot-resource-server/src/main/java/com/saran/controllers/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.saran.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.saran.model.Employee;
+
+@Controller
+public class EmployeeController {
+
+    @RequestMapping(value = "/user/getEmployeesList", produces = "application/json")
+    @ResponseBody
+    public List<Employee> getEmployeesList() {
+        List<Employee> employees = new ArrayList<>();
+        Employee emp = new Employee();
+        emp.setEmpId("1001");
+        emp.setEmpName("Emre");
+        employees.add(emp);
+        return employees;
+
+    }
+
+}
